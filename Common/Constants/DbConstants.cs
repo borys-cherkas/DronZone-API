c8156205fa777ad1af6685681432ecedc1c5c393
Borys Cherkas <borys.cherkas@nure.ua>
2018-12-10 00:29:00 +0200

    Fixed db connection string  to Azure

--- Common/Constants/DbConstants.cs
+++ Common/Constants/DbConstants.cs
@@ -4,6 +4,6 @@
     {
         //public const string LocalConnectionString = "Server=(localdb)\\mssqllocaldb;Database=dronezonedb;Trusted_Connection=True;";
 
-        public const string AzureConnectionString = "Server=tcp:dronzone-db.csvsyoxrmhzk.us-east-2.rds.amazonaws.com,1433; Initial Catalog=DronZoneDB;Persist Security Info=False;User ID=admin;Password=Test123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public const string AzureConnectionString = "Server=tcp:dronzone-db.database.windows.net,1433;Initial Catalog=DronZoneDB;Persist Security Info=False;User ID=borys-cherkas;Password=Test123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
     }
 }
